4d8b22d585759b5a783fa00619034ccd17953627
Johan Ju <johanj93@gmail.com>
2015-08-07 19:31:31 +0200

    test windows

--- java/com/othello/Main.java
+++ java/com/othello/Main.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//test windows 10
 package com.othello;
 
 import android.app.Activity;
